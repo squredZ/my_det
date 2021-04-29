731c348955c981e6cb73276794d969b61f826a33
sbisdog <demonsoldier@163.com>
2021-04-29 18:20:00 +0800

    add small sample

---
+++ auto_learn/get_dif_sample.py
@@ -0,0 +1,8 @@
+import os
+import json
+from  pycocotools.coco import COCO
+
+import torch
+import torch.nn as nn
+
+"C:\Users\zhangze\AppData\Roaming\CCwork\7652669648891056\FileRecv\xtrzk".replace("\","/")
